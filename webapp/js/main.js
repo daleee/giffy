bf7ea75c064859b17c11c836eb929ec796e61cca
Dale Karp <dale@dale.io>
2014-07-14 02:37:48 -0400

    add support for s3 stuff in main.js

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -1,23 +1,39 @@
 var giffyApp = angular.module('giffyApp', []);
 
-giffyApp.factory('S3Service', function() {
-    var numOfGifs = 0;
+giffyApp.factory('S3Service', function($http) {
+    function getListOfGifs (){
+        return $http({method: 'GET', url: 'http://localhost:8080/'});
+    }
+
     return {
-        numOfGifs: numOfGifs
+        getListOfGifs: getListOfGifs
     };
 });
 
 function IndexCtrl($scope, S3Service) {
-    var numOfGifs = S3Service.numOfGifs;
+    $scope.numOfGifs = 0;
+    $scope.gifList = [];
 
-    $scope.getNumOfGifs = function() {
-        return numOfGifs;
-    };
+    var gifGetter = S3Service.getListOfGifs();
+    gifGetter.
+        success(function(data){
+            $scope.numOfGifs = data.length;
+            $scope.gifList = data;
+        }).
+        error(function(data, status, headers,config) {
+            //TODO: remove
+            console.log("ERROR: There was an error asking S3 for funnies: ");
+            console.log(data);
+            console.log(status);
+            console.log(headers);
+            console.log(config);
+        });
 
+    //TODO: turn into directive
     function s3_upload(){
         var s3upload = new S3Upload({
             file_dom_selector: 'gifUploadBtn',
-            s3_sign_put_url: '/sign_s3',
+            s3_sign_put_url: 'http://localhost:8080/sign_s3',
             onProgress: function(percent, message) {
                 console.log('%s : %s', percent, message);
             },
