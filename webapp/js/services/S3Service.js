be29b6d51763bcd0875f10f6462f66b81dcc5629
Dale Karp <dale@dale.io>
2014-09-23 06:02:32 -0400

    WEBAPP: moved S3Service code into its own file

---
+++ webapp/js/services/S3Service.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('giffy')
+    .factory('S3Service', ['$http', function($http) {
+        function getListOfGifs (){
+            return $http({method: 'GET', url: 'http://localhost:8080/'});
+        }
+
+        function getGif(name) {
+            return $http({method: 'GET', url: 'http://localhost:8080/gifs/' + name});
+        }
+
+        function getLatestGifs (){
+            //TODO: implement on server
+            return $http({method: 'GET', url: 'http://localhost:8080/latest'});
+        }
+
+        function registerGifWithAPI () {
+            // TODO: make api call to register db entry for img
+        }
+
+        return {
+            getListOfGifs: getListOfGifs,
+            getGif: getGif
+        };
+    }]);
