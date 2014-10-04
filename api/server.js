9f3dd9c623c6957c3ab2086991a796feb9559ed1
Dale Karp <dale@dale.io>
2014-10-04 04:34:55 -0400

    some reindenting

--- api/server.js
+++ api/server.js
@@ -7,21 +7,21 @@ var aws = require('aws-sdk');
 var apiOptions, awsOptions, dbOptions;
 try {
     apiOptions = require('./conf/api');
-} catch(e) {
+} catch (e) {
     console.log('ERROR: API configuration is missing!');
     console.log('Please rename conf/api.js.example to conf/api.js and edit the file to your needs.');
     process.exit(1);
 }
 try {
     awsOptions = require('./conf/aws');
-} catch(e) {
+} catch (e) {
     console.log('ERROR: AWS configuration is missing!');
     console.log('Please rename conf/aws.js.example to conf/aws.js and edit the file to your needs.');
     process.exit(1);
 }
 try {
     dbOptions = require('./conf/db');
-} catch(e) {
+} catch (e) {
     console.log('ERROR: DB configuration is missing!');
     console.log('Please rename conf/db.js.example to conf/db.js and edit the file to your needs.');
     process.exit(1);
@@ -36,7 +36,7 @@ knex.raw('select 1+1 as result')
     .then(function (result) {
         // it worked... silent pass!
     })
-.catch(function (error) {
+    .catch(function (error) {
         console.log('ERROR: Could not access your database. Check the config and make sure SQL is running!');
         process.exit(1);
     });
@@ -53,6 +53,6 @@ var models = require('./models')(bookshelf);
 var routes = require('./routes')(server, crypto, aws, models, awsOptions);
 
 // start server
-server.listen(apiOptions.port, function() {
+server.listen(apiOptions.port, function () {
     console.log('Giffy API is now online at: %s', server.url);
 });
